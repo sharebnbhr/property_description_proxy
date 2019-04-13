911d527522a9dd19ec4c2c468a3952e4cddc9c01
Anthony Kim <tony@Anthonys-MacBook-Pro.local>
2019-04-12 17:15:22 -0700

    modified submodule

--- property_description/database/dbHelpers.js
+++
@@ -1,8 +0,0 @@
-const Property = require('./');
-
-getPropertyHelper = (num) => Property.find({}).limit(1).skip(num)
-// getPropertyHelper = () => Property.findOne()
-// getPropertyHelper = () => Property.find({})
-
-
-module.exports = getPropertyHelper
--- property_description/server/router.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const controller = require('./controller.js');
-
-//req.body
-router
-  .route('/desc')
-  .get(controller.get)
-
-module.exports = router
